b2958b22e3c404347849d72a98c82289e127079f
Hedra <Hedra_gerges@live.com>
2020-03-16 12:45:51 -0500

    Spring Boot Initial application

---
+++ backEnd/src/main/java/CS544/finalProject/backEnd/BackEndApplication.java
@@ -0,0 +1,13 @@
+package CS544.finalProject.backEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/CS544/finalProject/backEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package CS544.finalProject.backEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
