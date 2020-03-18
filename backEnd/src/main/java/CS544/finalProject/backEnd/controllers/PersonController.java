8fe42fe77eccf7cb98e69f3bbb2809fba5219f67
Tuugii <Tuugii@172.19.140.90>
2020-03-18 17:55:24 -0500

    Used for enrollment update function. Just to avoid conflict.

--- backEnd/src/main/java/CS544/finalProject/backEnd/controllers/PersonController.java
+++ backEnd/src/main/java/CS544/finalProject/backEnd/controllers/PersonController.java
@@ -1,11 +1,37 @@
 package CS544.finalProject.backEnd.controllers;
 
-import CS544.finalProject.backEnd.models.Person;
-import org.springframework.web.bind.annotation.PostMapping;
+import CS544.finalProject.backEnd.models.Enrollment;
+import CS544.finalProject.backEnd.models.Student;
+import CS544.finalProject.backEnd.models.StudentSectionDTO;
+import CS544.finalProject.backEnd.services.EnrollmentService;
+import CS544.finalProject.backEnd.services.StudentService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-public class PersonController {
-
+public class PersonController {	
+	@Autowired
+	private EnrollmentService enrollmentService;
+//	@Autowired StudentService stuService;
+//	
+//	
+//	@PutMapping(value="/students/{studentId}/updateenrollment")
+//	public Student saveEnrollment(@PathVariable Long studentId, @RequestBody List<Enrollment> enrollments) throws Throwable
+//	{
+//		Student student = stuService.findById(studentId);
+//		student.setEnrollments(enrollments);
+//		return stuService.update(studentId, student);
+//	}
+	
+	@PutMapping(value="/updateenrollment/{enrollId}")
+	public Enrollment updateEnrollment(@PathVariable Long enrollId, @RequestBody Enrollment enrollment) throws Throwable
+	{
+		return enrollmentService.update(enrollId, enrollment);
+	}
 
 }
